b27a7be4baed9fe2a2adb12b67689870495703a5
safal <tsafal06@gmail.com>
2022-11-14 13:09:58 +0545

    created an object with its properties

---
+++ index.js
@@ -0,0 +1,11 @@
+/* creating an objcet named product that contains properties: id,
+  name, color,size,price. */
+let product={
+  id : '01',
+  name : 'Shoes',
+  color:'red',
+  size: '4',
+  price:'1200'
+};
+ 
+console.log(product);
